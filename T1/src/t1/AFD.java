2f7b762609d4f810d6e63dc0ce2a9f4e8f333b4b
maticou <maticou123@gmail.com>
2019-10-22 15:53:05 -0300

    arreglo afd2

--- T1/src/t1/AFD.java
+++ T1/src/t1/AFD.java
@@ -14,7 +14,50 @@ import java.util.Stack;
  */
 public class AFD {
     
+    Estado estado;
+    Transicion transicion;
+
+    ArrayList<Estado> listaEstados;
+    ArrayList<Transicion> listaTransiciones;
+    ArrayList<String> alfabeto;
+    ArrayList<String> inicios;
+    ArrayList<String> finales;
+    Automata afnd;
+    ArrayList<Estado> estados_epsilon;
+    
     public AFD(Automata automata) {
-          
+        
+        //this.transicion = new Transicion();
+
+        this.listaEstados = new ArrayList();
+        this.listaTransiciones = new ArrayList();
+        this.alfabeto = new ArrayList();
+        this.inicios = new ArrayList();
+        this.finales = new ArrayList();
+
+        this.afnd = automata;
+        estados_epsilon =  new ArrayList<Estado>();
+        obtener_eclosure();          
     }          
+    
+    void convertir_automata(){
+
+    }
+
+    void obtener_eclosure(){
+        transicion_epsilon(afnd.inicio);
+    }
+
+    void transicion_epsilon(Estado estado){
+
+        if(estados_epsilon.contains(estado) == false){
+            this.estados_epsilon.add(estado);
+
+            if(estado.transiciones.get('Ɛ') != null){
+                for(Estado s: estado.transiciones.get('Ɛ')){
+                    transicion_epsilon(s);
+                }
+            }
+        }
+    }
 }
